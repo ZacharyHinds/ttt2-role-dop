278bfb59407b2c60dfe5d1a902613f202a780648
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-08-24 16:09:34 -0400

    Fixed role syncing to hide the Doppelganger better

--- lua/terrortown/autorun/server/sv_doppel_handler.lua
+++ lua/terrortown/autorun/server/sv_doppel_handler.lua
@@ -20,18 +20,27 @@ local function DoppelChange(ply, key)
     new_team = TEAM_DOPPELGANGER
   end
 
-  new_role, new_team, did_steal = hook.Run("TTT2DoppelgangerRoleChange", ply, new_role, new_team, did_steal)
+  new_role, new_team, did_steal = hook.Run("TTT2DoppelgangerRoleChange", ply, new_role, new_team, did_steal) or new_role, new_team, did_steal
 
+  print("[TTT2 Doppelganger] Role: " .. new_role)
+  print("[TTT2 Doppelganger] Team: " .. new_team)
+  if did_steal then
+    print("[TTT2 Doppelganger] Did Steal: True")
+  else
+    print("[TTT2 Doppelganger] Did Steal: False")
+  end
   ply:SetRole(new_role, new_team)
+  SendFullStateUpdate()
+  ply:UpdateTeam(new_team)
   SendFullStateUpdate()
 
   if not did_steal then return end
-  local steal_mode = GetConVar("ttt2_dop_steal_role")
+  local steal_mode = GetConVar("ttt2_dop_steal_role"):GetBool()
 
   if steal_mode and AMNESIAC then
     tgt:SetRole(ROLE_AMNESIAC, TEAM_NONE)
+    SendFullStateUpdate()
   end
-  SendFullStateUpdate()
 
   local popup_mode = GetConVar("ttt2_dop_declare_mode")
 
@@ -50,8 +59,39 @@ local function DoppelChange(ply, key)
   end
 
 end
-
 hook.Add("KeyPress", "TTT2DoppelChange", DoppelChange)
+
+hook.Add("TTT2SpecialRoleSyncing", "TTT2RoleDopMod", function(ply, tbl)
+  if GetRoundState() == ROUND_POST then return end
+  if ply:GetSubRoleData().unknownTeam then return end
+
+  local dopSelected = false
+
+  for dop in pairs(tbl) do
+    -- if dop == ply then continue end
+    if dop:IsTerror() and dop:Alive() and dop:GetTeam() == TEAM_DOPPELGANGER and dop:GetSubRole() ~= ROLE_DOPPELGANGER then
+      if dop ~= ply then
+        tbl[dop] = {dop:GetBaseRole(), dop:GetSubRoleData().defaultTeam}
+      else
+        tbl[dop] = {dop:GetSubRole(), TEAM_DOPPELGANGER}
+      end
+      dopSelected = true
+    end
+  end
+
+  if dopSelected and ply:GetTeam() == TEAM_DOPPELGANGER then
+    -- local GetPlayers = player.GetAll()
+    -- local count = #GetPlayers
+    for teammate in pairs(tbl) do
+      if teammate == ply then continue end
+      if teammate:GetTeam() == TEAM_DOPPELGANGER then continue end
+      if not teammate:IsTerror() or not teammate:Alive() then continue end
+      if teammate:HasTeam(ply:GetSubRoleData().defaultTeam) then
+        tbl[teammate] = {teammate:GetSubRole(), teammate:GetTeam()}
+      end
+    end
+  end
+end)
 
 local function DoppelMarker(ply, new_role, new_team, did_steal)
   if not MARKER then return end
--- lua/terrortown/entities/roles/doppelganger/shared.lua
+++ lua/terrortown/entities/roles/doppelganger/shared.lua
@@ -40,7 +40,7 @@ function CorrectDopPly(ply)
   if ply:GetSubRole() ~= ROLE_DOPPELGANGER then return end
   if ply:GetTeam() ~= TEAM_NONE then return end
 
-  ply:UpdateTeam(TEAM_DOPPELGANGER)
+  ply:SetRole(ROLE_DOPPELGANGER, TEAM_DOPPELGANGER)
 end
 
 function CorrectDopTeam()
